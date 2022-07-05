13a00c68c5895f1aa670e2699508210f8139465a
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-07-05 20:11:29 +0400

    adding redirects based on auth status

--- SpotiZen/frontend/views.py
+++ SpotiZen/frontend/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from requests import get
 
+from spotify.utils import isAuthenticated
+
 def welcome(request):
+	session_id = request.session.session_key
+	if isAuthenticated(session_id):
+		return redirect('frontend-main')
 	return render(request, 'frontend/home.html')
 
 def main(request):
-	return render(request, 'frontend/main.html')
+	session_id = request.session.session_key
 
-	
+	if not isAuthenticated(session_id):
+		return redirect('frontend-welcome')
+	return render(request, 'frontend/main.html')
