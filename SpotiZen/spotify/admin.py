a80b363ee628374a7c97320d15fa43d89d8ab0ab
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-07-05 21:34:49 +0400

    changes to frontend

--- SpotiZen/spotify/admin.py
+++ SpotiZen/spotify/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SpotifyToken
+
+@admin.register(SpotifyToken)
+class SpotifyToken(admin.ModelAdmin):
+	list_display = ('id', 'user', 'access_token', 'refresh_token', 'token_type', 'expires_in', 'created_at')
+	fields = ['user', 'access_token', 'refresh_token', 'token_type', 'expires_in']
