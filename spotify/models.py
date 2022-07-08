5c73a181d3e4390c31bae0ae1d9b156b04c20262
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-07-08 15:46:52 +0400

    converting charfield to textfield in SpotifyToken model

--- spotify/models.py
+++ spotify/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class SpotifyToken(models.Model):
 	user = models.CharField(max_length=50, unique=True, null=True)
 	created_at = models.DateTimeField(auto_now_add=True, null=True)
-	access_token = models.CharField(max_length=150, null=True)
+	access_token = models.TextField(max_length=150, null=True)
 	token_type = models.CharField(max_length=50, null=True)
 	expires_in = models.DateTimeField(null=True)
-	refresh_token = models.CharField(max_length=50, null=True)
+	refresh_token = models.TextField(max_length=50, null=True)
